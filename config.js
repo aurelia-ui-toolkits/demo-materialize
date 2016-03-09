88b34c409e27ed12c8db679a13148051a3063a3c
Daniel Bendel <daniel.bendel@gmail.com>
2016-03-09 07:48:11 +0100

    Update 2016-03-09T06:47:44.733Z

--- config.js
+++ config.js
@@ -446,7 +446,7 @@ System.config({
       "github:components/jquery@2.1.4",
       "github:components/jquery@2.1.4/jquery"
     ],
-    "app-build-4d6c6edad9": [
+    "app-build-7f8144896d": [
       "about/about",
       "about/about.html!github:systemjs/plugin-text@0.0.4",
       "app",
