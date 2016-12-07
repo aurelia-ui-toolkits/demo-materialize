0d47af2ce78cf5d298c42d2074ba8e0c11424266
Daniel Bendel <daniel.bendel@gmail.com>
2016-12-07 10:20:22 +0100

    Update 2016-12-07T09:19:48.398Z

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // import 'materialize';
 
 //Configure Bluebird Promises.
-// (Promise.config || P.config)({
-//   warnings: {
-//     wForgottenReturn: false
-//   }
-// });
+(Promise.config || P.config)({
+  warnings: {
+    wForgottenReturn: false
+  }
+});
 
 export function configure(aurelia) {
   return System.import('materialize').then(() => {
