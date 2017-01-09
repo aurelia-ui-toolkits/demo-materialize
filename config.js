8162387d6bd10fca5eb901212f9810896b19fd5d
Daniel Bendel <daniel.bendel@gmail.com>
2017-01-09 21:11:01 +0100

    Update 2017-01-09T20:10:52.645Z

--- config.js
+++ config.js
@@ -583,7 +583,7 @@ System.config({
       "npm:jquery@2.2.4.js",
       "npm:jquery@2.2.4/dist/jquery.js"
     ],
-    "app-build-65692dd60e.js": [
+    "app-build-8a3e6a5af5.js": [
       "about/about.html!github:systemjs/plugin-text@0.0.4.js",
       "about/about.js",
       "app-colors.html!github:systemjs/plugin-text@0.0.4.js",
