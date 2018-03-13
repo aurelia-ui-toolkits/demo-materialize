f6416241a3490caf34c77ef1e85f68fb3d0a84ea
Max <m.balaganskiy@gmail.com>
2018-03-13 13:59:37 +1100

    Upgrade to bridge v0.33.0

--- src/about/about.ts
+++ src/about/about.ts
@@ -30,7 +30,7 @@ export class About {
 	attached() {
 		// let bridge = System.get(System.normalizeSync('aurelia-materialize-bridge'));
 		// this.version = bridge.version;
-		this.version = "0.32.0";
+		this.version = "0.33.0";
 	}
 
 	onSelectionChanged(e) {
